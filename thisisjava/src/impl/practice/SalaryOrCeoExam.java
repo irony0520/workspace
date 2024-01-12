11b478f61b874242e504a02f676c78eaf08fccd4
irony0520 <153781177+irony0520@users.noreply.github.com>
2024-01-12 18:45:50 +0900

    imfuckingdeadman

---
+++ thisisjava/src/impl/practice/SalaryOrCeoExam.java
@@ -0,0 +1,34 @@
+package impl.practice;
+
+public class SalaryOrCeoExam {
+
+	
+	
+	public static void main(String[] args) {
+		
+		SalaryMan salary = new SalaryMan("ㅇ",23,304943);
+		System.out.println(salary.getInfo());
+		System.out.println(SalaryTaxPayer(salary));
+		
+
+		BusinessMan Business = new BusinessMan("ㅇ",23,304943,234);
+		System.out.println(Business.getInfo());
+		System.out.println(BusinessTaxPayer(Business));
+	}
+	
+
+
+private static double SalaryTaxPayer(SalaryMan s) {
+	  return s.calcTax();
+	}
+
+
+
+private static double BusinessTaxPayer(BusinessMan b) {
+	  return b.calcTax();
+	}
+}
+
+
+
+//payer 부분 배열로 만들기 어케함??????????????????????????????????
